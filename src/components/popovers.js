82ec6d75cc42d12acda58be1439a6f6a902f6849
Dana Simmons <dana@simmons-audio.com>
2019-04-30 11:53:41 -0400

    popovers are arrow functions that we can pass props too

--- src/components/popovers.js
+++ src/components/popovers.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {Popover, FormControl, Button} from 'react-bootstrap'
 
 
-export const NewPiPopover = (
-    <Popover id="popover-basic" title="New Item Popover">
+export const NewPiPopover = (props)=>(
+    <Popover {...props} id="popover-basic" title="New Item Popover">
         Lets make a new item
         <FormControl type="text" placeholder="What is it?" />
         <FormControl type="number" placeholder="How much is there?" />
@@ -11,3 +11,16 @@ export const NewPiPopover = (
         <Button>New</Button>
     </Popover>
 )
+
+export const NewPrPopover = (props) => (
+    <Popover {...props} id="popover-basic" title="New Item Popover">
+        Lets make a new item
+        <FormControl type="text" placeholder="What is it?" />
+        <FormControl type="number" placeholder="How much is there?" />
+        <FormControl type="text" placeholder="Unit size: bag/box/cup/ounce/etc.." />
+        <Button>New</Button>
+    </Popover>
+)
+
+
+
