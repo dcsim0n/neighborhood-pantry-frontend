7bc48a4d176ce5c157f3925a356674823f287676
Dana Simmons <dana@simmons-audio.com>
2019-04-30 16:48:17 -0400

    added fontawesome icons to pantry cards

--- src/components/PantryCard.js
+++ src/components/PantryCard.js
@@ -2,8 +2,8 @@ import React from 'react'
 import {Card} from 'react-bootstrap'
 export default function PantryCard(props) {
   return (
-    <Card style={{width:"8em", display:"inline-block"}}>
-        <Card.Title>{props.card.name}</Card.Title>
+    <Card style={{width:"12em", display:"inline-block"}}>
+        <Card.Title>{props.card.name} <i className="fas fa-trash-alt"></i> <i class="fas fa-info-circle"></i></Card.Title>
         <ul>
           <li>{props.card.quantity}: {props.card.unit}</li>
           <li>{props.card.user.first_name} {props.card.user.last_name}</li>
