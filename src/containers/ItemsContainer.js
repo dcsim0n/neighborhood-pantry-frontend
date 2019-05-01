e8c8991e02c2d0694c529db8b1fd2aa252440a41
Dana Simmons <dana@simmons-audio.com>
2019-05-01 09:21:25 -0400

    split the activity container into new sub components ItemsContainer and RequestsContainer encapsulate request and item functionality

--- src/containers/ItemsContainer.js
+++ src/containers/ItemsContainer.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux'
+import {Col, Row, Container,ButtonGroup, OverlayTrigger, DropdownButton, Dropdown, Button } from 'react-bootstrap';
 
+import {connect} from 'react-redux'
+import {newPI} from '../actions/actions';
+import {NewPiPopover} from '../components/popovers';
 import ItemCard from '../components/ItemCard';
 
 class ItemsContainer extends Component {
-  render() {
+  handlePiForm = (event) => {
+    event.preventDefault()
+    console.log('event.target.name', event.target.name.value)
+    const {name, quantity, unit} = event.target
+    
+    const pantryItem ={
+        name: name.value,
+        quantity: quantity.value,
+        unit: unit.value
+    }
+    console.log('pantryItem :', pantryItem);
+    this.props.dispatch(newPI(pantryItem))
+  }
+  render(){
     return (
-      <div>
-        {this.props.items.map(card=><ItemCard key={card.id} card={card}/>)}
+      <div className="border">
+          <Container>
+              <Row> 
+                  <Col lg="3">
+                    <h3>Pantry Items</h3>
+                  </Col>
+                  <Col>
+                    <ButtonGroup>
+                      <OverlayTrigger trigger="click" placement="right" overlay={NewPiPopover({handleSubmit:this.handlePiForm})}>
+                        <Button variant="secondary">New Item</Button>
+                      </OverlayTrigger>
+                      <DropdownButton as={ButtonGroup} variant="secondary" title="Sort">
+                        <Dropdown.Item  eventKey="1">Newest First</Dropdown.Item>
+                        <Dropdown.Item eventKey="1">Oldest First</Dropdown.Item>
+                      </DropdownButton>
+                      <Button variant="secondary">Show More</Button>
+                    </ButtonGroup>
+                  </Col>
+              </Row>
+          </Container>
+          <div >
+            {this.props.items.map(card=><ItemCard key={card.id} card={card}/>)}
+          </div>
       </div>
     )
-  }
+  }  
 }
+
 
 const mapStateToProps = (state, ownProps) => {
     return {
